3f37742a12ae134ae01cfa1ecbfb6fbaa846c51e
Justin Cole <justincole@outlook.co.nz>
2020-11-25 20:57:09 +1300

    (feat) game completed

--- game.js
+++ game.js
@@ -1,45 +1,63 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener("DOMContentLoaded", start);
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+function start() {
+	bindEventListeners(document.getElementsByClassName("board")[0].children);
 }
 
-function bindEventListeners (dots) {
-  for (var i = 0; i < dots.length; i++) {
-    // BIND YOUR EVENT LISTENERS HERE
-    // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-  }
+function bindEventListeners(dots) {
+	for (var i = 0; i < dots.length; i++) {
+		// BIND YOUR EVENT LISTENERS HERE
+		// The first one is provided for you
+		dots[i].addEventListener("contextmenu", makeGreen);
+		dots[i].addEventListener("click", makeBlue);
+		dots[i].addEventListener("dblclick", hide);
+	}
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+	evt.preventDefault();
+	evt.target.classList.toggle("green");
+	evt.target.classList.remove("blue");
+	evt.target.classList.remove("invisible");
+	updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+const makeBlue = (evt) => {
+	// evt.preventDefault();
+	evt.target.classList.toggle("blue");
+	evt.target.classList.remove("green");
+	evt.target.classList.remove("invisible");
+	updateCounts();
+};
 // CREATE FUNCTION hide HERE
+const hide = (evt) => {
+	evt.target.classList.toggle("invisible");
+	evt.target.classList.remove("green");
+	evt.target.classList.remove("blue");
+	updateCounts();
+};
 
-function updateCounts () {
-  var totals = {
-    blue: 0,
-    green: 0,
-    invisible: 0
-  }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+function updateCounts() {
+	var totals = {
+		blue: 0,
+		green: 0,
+		invisible: 0,
+	};
 
-  // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+	// WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+	totals.blue = document.getElementsByClassName("blue").length;
+	totals.green = document.getElementsByClassName("green").length;
+	totals.invisible = document.getElementsByClassName("invisible").length;
+	// Once you've done the counting, this function will update the display
+	displayTotals(totals);
 }
 
-function displayTotals (totals) {
-  for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
-  }
+function displayTotals(totals) {
+	for (var key in totals) {
+		document.getElementById(key + "-total").innerHTML = totals[key];
+	}
 }
